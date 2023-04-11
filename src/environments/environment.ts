1d06b04fddd266e2f11638dcd7a217eacabf17ce
Leonardo Cserny <leo.cserny@gmail.com>
2023-04-11 21:10:14 +0300

    minor change port

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    commanderApiUrlBase: "http://localhost:9000/api",
+    commanderApiUrlBase: "http://localhost:8080/api",
     fallbackPosterUrl: "/assets/img/no-poster.jpg",
     darkModeStartHour: 20,
     darkModeEndHour: 7,
