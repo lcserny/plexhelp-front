444579e25bc13b6dc2526fa43737115bdf33b18e
Leonardo Cserny <leo.cserny@gmail.com>
2024-10-02 14:11:12 +0300

    minor fix

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,8 @@
+const currentDomain = `${window.location.protocol}//${window.location.hostname}`;
+
 export const environment = {
-    commanderApiUrl: 'http://localhost:8080/api/v1',
-    securityApiUrl: 'http://localhost:8081/security',
+    commanderApiUrl: `${currentDomain}:8080/api/v1`,
+    securityApiUrl: `${currentDomain}:8081/security`,
 
     fallbackPosterUrl: "/assets/img/no-poster.jpg",
     darkModeStartHour: 20,
