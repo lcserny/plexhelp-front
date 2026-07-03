14d66906708a105d8ff1dc697df1e5182e2e272e
Leonardo Cserny <leo.cserny@gmail.com>
2026-07-03 09:04:39 +0300

    adds auto clear of pinned loading

--- src/app/loading.service.spec.ts
+++ src/app/loading.service.spec.ts
@@ -1,4 +1,4 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, fakeAsync, tick } from '@angular/core/testing';
 
 import { LoadingService } from './loading.service';
 
@@ -13,4 +13,36 @@ describe('LoadingService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should unpin after 1 minute', fakeAsync(() => {
+    service.setPinnedLoading(true);
+    expect(service.getLoading()).toBe(true);
+
+    tick(60000);
+
+    expect(service.getLoading()).toBe(false);
+  }));
+
+  it('should not unpin if setPinnedLoading(false) called before 1 minute', fakeAsync(() => {
+    service.setPinnedLoading(true);
+    expect(service.getLoading()).toBe(true);
+
+    tick(30000);
+    service.setPinnedLoading(false);
+
+    tick(30000);
+    expect(service.getLoading()).toBe(false);
+  }));
+
+  it('should reset timer if setPinnedLoading(true) called again', fakeAsync(() => {
+    service.setPinnedLoading(true);
+    tick(30000);
+    service.setPinnedLoading(true); // reset timer
+    
+    tick(40000); // 30s + 40s = 70s passed since first start, but timer was reset at 30s
+    expect(service.getLoading()).toBe(true); // Should still be true
+    
+    tick(30000); // 70s + 30s = 100s, total 100s. Should be false now
+    expect(service.getLoading()).toBe(false);
+  }));
 });
--- src/app/loading.service.ts
+++ src/app/loading.service.ts
@@ -5,8 +5,12 @@ import { Injectable } from '@angular/core';
 })
 export class LoadingService {
 
+    private static readonly timeoutMs = 60000;
+
     private loading: boolean = false;
     private pinned: boolean = false;
+    private pinnedTimestamp: number | null = null;
+    private timer: any = null;
 
     constructor() { }
 
@@ -18,11 +22,36 @@ export class LoadingService {
     }
 
     setPinnedLoading(loading: boolean) {
+        this.resetTimerIfNeeded();
+
         this.pinned = loading;
         this.loading = loading;
+
+        if (loading) {
+            this.pinnedTimestamp = Date.now();
+            this.scheduleTimer();
+        } else {
+            this.pinnedTimestamp = null;
+        }
     }
 
     getLoading(): boolean {
         return this.loading;
+    }
+
+    private scheduleTimer() {
+        this.timer = setTimeout(() => {
+            if (this.pinnedTimestamp && (Date.now() - this.pinnedTimestamp >= LoadingService.timeoutMs)) {
+                this.pinned = false;
+                this.loading = false;
+            }
+        }, LoadingService.timeoutMs);
+    }
+
+    private resetTimerIfNeeded() {
+        if (this.timer) {
+            clearTimeout(this.timer);
+            this.timer = null;
+        }
     }
 }
